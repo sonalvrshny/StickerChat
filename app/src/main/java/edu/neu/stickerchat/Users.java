7add9525482fa83b024813c6446b7817246c85f3
Sonal Varshney <sonalvrshny@gmail.com>
2022-04-12 21:16:11 -0400

    registered user

---
+++ app/src/main/java/edu/neu/stickerchat/Users.java
@@ -0,0 +1,30 @@
+package edu.neu.stickerchat;
+
+public class Users {
+    String uid;
+    String username;
+
+    public Users() {
+    }
+
+    public Users(String uid, String username) {
+        this.uid = uid;
+        this.username = username;
+    }
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
