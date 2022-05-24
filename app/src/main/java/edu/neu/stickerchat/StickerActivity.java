53d6e22baac342b4746157227e81de968db1caa9
Sonal Varshney <sonalvrshny@gmail.com>
2022-05-24 12:25:39 -0400

    added stickers

--- app/src/main/java/edu/neu/stickerchat/StickerActivity.java
+++ app/src/main/java/edu/neu/stickerchat/StickerActivity.java
@@ -1,14 +1,61 @@
 package edu.neu.stickerchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Pair;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class StickerActivity extends AppCompatActivity {
+
+    private FirebaseDatabase database;
+    private FirebaseAuth auth;
+    private List<Pair<String,String>> stickerList;
+    private StickerAdapter stickerAdapter;
+    private RecyclerView stickerRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sticker);
+
+        String recID = getIntent().getStringExtra("recID");
+
+        stickerList = new ArrayList<>();
+        auth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference().child("stickers");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String key = dataSnapshot.getKey().toString();
+                    String val = dataSnapshot.getValue().toString();
+                    stickerList.add(new Pair<>(key, val));
+                }
+                stickerAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+        stickerRecycler = findViewById(R.id.sticker_recycler);
+        stickerRecycler.setLayoutManager(new GridLayoutManager(this, 3));
+        stickerAdapter = new StickerAdapter(this, stickerList, recID);
+        stickerRecycler.setAdapter(stickerAdapter);
     }
 }
---
+++ app/src/main/java/edu/neu/stickerchat/StickerAdapter.java
@@ -0,0 +1,63 @@
+package edu.neu.stickerchat;
+
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class StickerAdapter extends RecyclerView.Adapter<StickerAdapter.ViewHolder> {
+    private Context context;
+    private List<Pair<String, String>> mData;
+    private FirebaseDatabase database;
+    private FirebaseDatabase auth;
+    private String senderRoom, receiverRoom;
+    private String senderId, receiverId;
+
+    public StickerAdapter(Context context, List<Pair<String, String>> mData, String receiverId) {
+        this.context = context;
+        this.mData = mData;
+        this.receiverId = receiverId;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.sticker_card, parent, false);
+        return new StickerAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        String s = mData.get(position).second;
+        String emojiName = mData.get(position).first;
+        Uri uri = Uri.parse(s);
+        Log.e("Getting URI", "" + uri);
+        Picasso.get().load(uri).into(holder.mImage);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView mImage;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+            mImage = itemView.findViewById(R.id.sticker);
+        }
+    }
+}
