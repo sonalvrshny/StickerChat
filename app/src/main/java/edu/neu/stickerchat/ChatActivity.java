975872adcbe8cecb3320f9ccbd5e6f76e5f376b8
Sonal Varshney <sonalvrshny@gmail.com>
2022-05-24 11:43:18 -0400

    click button to send sticker

--- app/src/main/java/edu/neu/stickerchat/ChatActivity.java
+++ app/src/main/java/edu/neu/stickerchat/ChatActivity.java
@@ -1,14 +1,21 @@
 package edu.neu.stickerchat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,5 +47,39 @@ public class ChatActivity extends AppCompatActivity {
         receiverName = getIntent().getStringExtra("name");
         receiverDisplayName = findViewById(R.id.receiver_display);
         receiverDisplayName.setText(receiverName.split("@")[0]);
+
+        sendSticker = findViewById(R.id.enter_sticker_button);
+        messageRecyclerView = findViewById(R.id.MessageRecyclerView);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        messageRecyclerView.setLayoutManager(linearLayoutManager);
+        chatAdapter = new ChatAdapter(ChatActivity.this, messagesList);
+        messageRecyclerView.setAdapter(chatAdapter);
+
+//        DatabaseReference reference = database.getReference().child("user").child(senderId);
+        DatabaseReference chatReference = database.getReference().child("chats").child(senderRoom).child("messages");
+
+        chatReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                messagesList.clear();
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    Messages message = dataSnapshot.getValue(Messages.class);
+                    messagesList.add(message);
+                }
+                chatAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        sendSticker.setOnClickListener(v -> {
+            Intent intent = new Intent(ChatActivity.this, StickerActivity.class);
+            intent.putExtra("recId", receiverId);
+            startActivity(intent);
+        });
     }
 }
